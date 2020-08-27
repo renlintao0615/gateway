43b706c1e6178ddafe0eae8432646983aa1bda94
renlintao0615 <enyasida@163.com>
2020-08-27 20:21:15 +0800

    提交ssl 及配置

--- src/main/java/com/yyskt/controller/GatewayController.java
+++ src/main/java/com/yyskt/controller/GatewayController.java
@@ -1,6 +1,15 @@
 package com.yyskt.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +21,43 @@ import io.swagger.annotations.ApiOperation;
 
 @Api(value = "gateway测试")
 @RestController
-@RequestMapping("/yy")
 public class GatewayController {
-	
+
 	@Autowired
-	HttpServletRequest request;
+	private HttpServletResponse response; 
+	
 	
 	@ApiOperation(value = "session测试",tags = "session测试")
-	@GetMapping("/test")
-	public String test()
+	@GetMapping("/")
+	public InputStream test() throws IOException
 	{
-		Object attribute = request.getSession().getAttribute("AA");
-		return attribute.toString();
+		 response.setCharacterEncoding("utf-8");
+         response.setContentType("text/html");
+         PrintWriter out = response.getWriter();
+         // 第1步、使用File类找到一个文件
+         File f= new File("src/main/resources/index.html") ;    // 声明File对象
+         // 第2步、通过子类实例化父类对象
+         InputStream input = null ;    // 准备好一个输入的对象
+         input = new FileInputStream(f)  ;    // 通过对象多态性，进行实例化
+         BufferedReader bufferedReader = new BufferedReader(
+ 		new InputStreamReader(input));
+ 		String line = null;
+ 		while ((line = bufferedReader.readLine()) != null) { 
+             out.write(line);
+ 		}
+         // 第4步、关闭输出流
+         input.close() ;                        // 关闭输出流\
+         out.close();
+		return null;
 				
 	}
 
+
+	@ApiOperation(value = "测试",tags = "测试")
+	@GetMapping("/test")
+	public String test1() throws IOException
+	{
+		return "yes!";
+				
+	}
 }
--- src/main/java/com/yyskt/filter/SessionFilter.java
+++ src/main/java/com/yyskt/filter/SessionFilter.java
@@ -1,5 +1,8 @@
 package com.yyskt.filter;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
@@ -8,6 +11,9 @@ import com.netflix.zuul.exception.ZuulException;
 @Component
 public class SessionFilter extends ZuulFilter {
 	
+	@Autowired
+	private HttpServletRequest request;
+	
 	@Override
 	public boolean shouldFilter() {
 		// TODO Auto-generated method stub
@@ -15,6 +21,8 @@ public class SessionFilter extends ZuulFilter {
 	}
 	@Override
 	public Object run() throws ZuulException {
+		String requestURI = request.getRequestURI();
+		System.out.println(requestURI);
 		return null;
 	}
 	@Override
